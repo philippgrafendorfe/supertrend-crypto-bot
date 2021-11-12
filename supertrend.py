aeb270e4a351cefb1b34812cd3d601f3562459ca
LuckyOwel <94184075+LuckyOwel@users.noreply.github.com>
2021-11-12 17:04:58 +0100

    Update supertrend.py
    Test

--- supertrend.py
+++ supertrend.py
@@ -25,9 +25,7 @@ exchange = ccxt.bitpanda({
 
 MIN_WIN = 0.02
 TRADE_INVESTMENT_ETH = 0.02
-# hallo, ich bin omar
 
-# Nummer 068120214591
 def tr(data):
     data['previous_close'] = data['close'].shift(1)
     data['high-low'] = abs(data['high'] - data['low'])
